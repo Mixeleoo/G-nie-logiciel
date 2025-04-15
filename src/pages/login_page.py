1d95011d866037a6937e10cc10c3eaf2cc90a7a2
nono-1117 <okahingaku@gmail.com>
2025-04-15 14:16:41 +0200

    Je fais des trucs

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -9,7 +9,13 @@ class LoginPage(QWidget) :
         super().__init__(mainpage)
         self.ui = mainpage.ui
 
-        self.ui.back_button.clicked.connect(lambda : self.goto_homepage())
+        self.ui.back_button.clicked.connect(lambda : self.goto_homepage()) # bouton retour
+        self.ui.validate_connect_button.clicked.connect(lambda  : self.validate_connection())
+
+        # supression du message d'erreur
+        self.ui.error_label1.hide()
+        self.ui.email_line_connection.textEdited.connect(lambda : self.clear_error())
+        self.ui.password_line_connection.textEdited.connect(lambda : self.clear_error())
 
     def goto_homepage(self):
         '''
@@ -17,14 +23,23 @@ class LoginPage(QWidget) :
         :return: None
         '''
         self.ui.pages_logiciel.setCurrentIndex(0) # homepage
+        self.ui.error_label1.hide() #suppression du message d'erreur
 
     def validate_connection(self):
         '''
         Connecte l'utilisateur si son identifiant et mot de passe sont corrects
         :return: None
         '''
+        self.ui.error_label1.show()
 
-    def check_connection(self):
+    def clear_error(self):
+        '''
+        Enlève l'affichage de l'erreur quand l'utlisateur recommence à taper dans les input
+        :return: None
+        '''
+        self.ui.error_label1.hide() # suppression du message d'erreur
+
+    def check_connection(self) -> bool:
         '''
         Verifie que l'identifiant et mot de passe tapé par l'utilisateur sont correct
         :return: retourne True si corrects et False sinon
--- src/pages/sign_in_page.py
+++ src/pages/sign_in_page.py
@@ -10,6 +10,15 @@ class SignInPage(QWidget) :
         self.ui = mainpage.ui
 
         self.ui.back_button_2.clicked.connect(lambda : self.goto_homepage())
+        self.ui.validate_account_button.clicked.connect(lambda : self.validate_acccount())
+
+        # suppression messages d'erreur
+        self.ui.error_label2.hide()
+        self.ui.error_label3.hide()
+        self.ui.error_label4.hide()
+        self.ui.email_line_connection_2.textEdited.connect(lambda: self.clear_error())
+        self.ui.password_line_connection_2.textEdited.connect(lambda: self.clear_error())
+        self.ui.passwordconfirm_line_connection_2.textEdited.connect(lambda: self.clear_error())
 
     def goto_homepage(self):
         '''
@@ -17,3 +26,37 @@ class SignInPage(QWidget) :
         :return: None
         '''
         self.ui.pages_logiciel.setCurrentIndex(0) # homepage
+        # suppression des messages d'erreur
+        self.ui.error_label2.hide()
+        self.ui.error_label3.hide()
+        self.ui.error_label4.hide()
+
+    def validate_acccount(self):
+        '''
+
+        :return:
+        '''
+        if not self.check_password_repeat() :
+            self.ui.error_label2.show()
+        elif self.ui.email_line_connection_2.text().strip() == "" :
+            self.ui.error_label3.show()
+        elif self.ui.password_line_connection_2.text().strip() == "" or self.ui.passwordconfirm_line_connection_2.text().strip() == "":
+            self.ui.error_label4.show()
+        else :
+            print("compte cree")
+
+    def check_password_repeat(self) -> bool:
+        '''
+        Vérifie si le mot de passe répété et bien le même
+        :return: True si oui et False sinon
+        '''
+        return self.ui.password_line_connection_2.text() == self.ui.passwordconfirm_line_connection_2.text()
+
+    def clear_error(self):
+        '''
+
+        :return:
+        '''
+        self.ui.error_label2.hide()
+        self.ui.error_label3.hide()
+        self.ui.error_label4.hide()
