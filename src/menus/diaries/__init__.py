8213c5bb23ef7ebb5652bfcd197560dc6c008895
leo <superrapace531@gmail;com>
2025-05-07 15:07:34 +0200

    on va prier

---
+++ src/menus/diaries/__init__.py
@@ -0,0 +1,7 @@
+from .diary_menu import DiaryMenu
+from .favorite_diary_menu import FavoriteDiaryMenu
+
+__all__ = [
+    "DiaryMenu",
+    "FavoriteDiaryMenu",
+]
--- src/pages/home_page.py
+++ src/pages/home_page.py
@@ -1,5 +1,9 @@
 from PyQt6.QtWidgets import QWidget
+<<<<<<< HEAD
 from src.main import MainWindow
+=======
+from ..main import MainWindow
+>>>>>>> 0ed2231 (on va prier)
 
 class HomePage(QWidget) :
     def __init__(self, mainpage : MainWindow):
